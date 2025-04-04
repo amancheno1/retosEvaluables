2ebbed78baf9aeb3ee60f2f017bf3537564bc4a6
amancheno1 <amancheno1979@gmail.com>
2025-04-04 19:58:43 +0200

    entrega Proyecto Final Modulo1

--- tema7/personTest.js
+++
@@ -1,9 +0,0 @@
-import Person
- from "./person.js";
- const hobbies = ["nadad","comer","dormir"];
- const person = new Person("Alejandro",1979, 90,1.85,hobbies);
- 
- console.log(person.calcImc());
- console.log(person.calcEdad());
- person.printAll();
- person.printhobbies();
